9db5865690be9b60fb18877d90e09a88ea3f4b64
Samhouuu <117370012+Samhouuu@users.noreply.github.com>
2023-01-29 19:00:07 -0500

    Fin Étape 3
    Création des tests redemarrerJEu

--- test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,35 @@
-// Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import { Joueur } from '../../../src/core/joueur';
+import supertest from 'supertest'
+import app from '../../../src/app';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("should implement test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+// Creation de 2 instances de joueur
+let joueur1 = new Joueur('yvan');
+let joueur2 = new Joueur('Maxime');
+
+const request = supertest(app);
+
+beforeAll(async () => {
+  joueur1 = new Joueur('yvan');
+  joueur2 = new Joueur('Maxime');
+});
+
+
+describe('GET /api/v1/jeu/redemarrerJeu/:id', () => {
+  it('verifie que les instances de joueurs ont ete creer', () => {
+    expect(joueur1).toBeDefined();
+    expect(joueur2).toBeDefined();
+  });
+
+  it("verifie que le statut de redemarrerJeu est 200 et que la reponse est du JSON", async () => {
+    const response = await request.post('/api/v1/jeu/redemarrerJeu').send({});
+    expect(response.status).toBe(200);
+    expect(response.type).toBe('application/json');
+  });
+
+  it('verifie que toutes les instances de joueurs ont été supprimées', () => {
+    expect(joueur1).toBeUndefined();
+    expect(joueur2).toBeUndefined();
   });
 });
